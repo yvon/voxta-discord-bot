e7dea8eb102e1a5815a602afa670921a7fca285c
Yvon Cognard <yvon@cognard.com>
2024-12-08 11:08:57 +0100

    feat: Add logger utility with info, error, and debug methods

---
+++ src/utils/logger.js
@@ -0,0 +1,7 @@
+const logger = {
+    info: (msg, ...args) => console.log(msg, ...args),
+    error: (msg, ...args) => console.error(msg, ...args),
+    debug: (msg, ...args) => console.debug(msg, ...args)
+};
+
+module.exports = logger;
